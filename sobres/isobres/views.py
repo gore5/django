aeb1ffc068db44dceec69bf5419a8909f4bb88de
Carolina Romeu Farre <crf1@pl-cap003.udl.net>
2013-04-03 15:43:50 +0200

    pujar algo

--- sobres/isobres/views.py
+++ sobres/isobres/views.py
@@ -1,1 +1,22 @@
 # Create your views here.
+
+from django.http import HttpResponse, Http404
+from django.template import Context
+from django.template.loader import get_template
+from django.contrib.auth.models import User
+
+def mainpage(request, username):
+	try:
+		user = User.objects.get(username=username)
+	except:
+		raise Http404('User not found.')
+
+
+	sobres = user.sobre_set.all()
+	template = get_template('userpage.html')
+	variables = Context({
+		'username': username,
+		'sobres': sobres
+		})
+	output = template.render(variables)
+	return HttpResponse(output)
--- sobres/sobres/settings.py
+++ sobres/sobres/settings.py
@@ -1,3 +1,4 @@
+import os.path
 # Django settings for sobres project.
 
 DEBUG = True
@@ -11,8 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'basedades.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -106,6 +107,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__),
+    '../isobres/templates'),
 )
 
 INSTALLED_APPS = (
@@ -115,8 +118,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'isobres',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
